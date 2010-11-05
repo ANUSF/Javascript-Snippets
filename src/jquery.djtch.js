dc15953280821c79280f59014f7f9df23aaa3fd6
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2010-11-05 15:04:47 +1100

    Added a copyright and license notice to the djtch code.

--- src/jquery.djtch.js
+++ src/jquery.djtch.js
@@ -1,9 +1,31 @@
+/*
+ * Author: Olaf Delgado-Friedrichs (odf@github.com)
+ * Copyright (c) 2010 ANU
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 /**
- * jquery.djtch.js - Dynamic Javascript Templating in Client-side HTML
+ * Dynamic Javascript Templating in Client-side HTML
  *
  * Requires jquery (>= 1.3) and jquery.forms
- *
- * @author olaf.delgado-friedrichs@anu.edu.au
  */
 
 /*jslint white: false, browser: true */
